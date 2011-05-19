69740d808757d5c4e2b221760b202b8c1be7e614
Dhananjay Ragade <dragade@linkedin.com>
2011-05-18 22:10:54 -0700

    removed api keys...

--- app/ApiKeys.scala
+++ app/ApiKeys.scala
@@ -7,6 +7,6 @@ package controllers
 object ApiKeys {
 
   // Get your own values from developer.linkedin.com -> Your Stuff  -> API Keys
-  val apiKey = "81c_ZrqefmsMsH7lbhN8SrAjn2ycdneANBIpX9BRQ9uWCcHHsjI0ECI1BUwT4Kwn"
-  val secretKey = "llNC8WGKrCQx-B9-qveFpvZzS9FU4fRk-6zrt2kNROx2vncpJePBMGbfVFZg9Q-o"
+  val apiKey = ""
+  val secretKey = ""
 }
